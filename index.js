2de58ce7b462cc714e02fed576e237fd1876324c
Sameer  Moyal <sameerab827@gmail.com>
2024-11-13 11:29:52 +0530

    create schema , model and get in browser

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express =require('express')
 const mongoose =require('mongoose')
 
-const app=express();
-const port=8989
-const mongoURL ='mongodb://localhost:27017'
 
-mongoose.connect(mongoURL).then(()=>console.log("successfully connected to  mongoDB")).catch((err)=>console.log("error to connect mongoDB",err))
+const port=8080
+const app=express()
+mongoose.connect('mongodb://localhost:27017')
+.then(()=>console.log("mongo successfully connected")).catch(()=>console.log("errrrrrrrrror",err))
 
 
-app.listen(port,()=>{
-    console.log(`server is runnig on this port ${port}`)
+const sales =mongoose.Schema({
+
+})  //create schema
+
+const salesData =mongoose.model('sales',sales)
+
+//http://localhost:8080/findAll
+const courseSchema =mongoose.Schema({
+
 })
 
+const courseModelData =mongoose.model('courses',courseSchema)
+
+
+
+app.get('/findAll',async(req,res)=>{
+    const mycourseData=await courseModelData.find()
+   
+    const mySalesData =await salesData.find()
+    res.status(200).json({course:mycourseData,sales:mySalesData})
+})
+
+app.listen(port,()=>console.log("server is run this port ",port))
